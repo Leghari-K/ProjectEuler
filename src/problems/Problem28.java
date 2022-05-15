df84c954349d795ec098e2d3554f20ae8646f029
Abdullah Leghari <leghari.k@gmail.com>
2022-05-16 04:45:15 +0500

    Problem28 attempt1

---
+++ src/problems/Problem28.java
@@ -0,0 +1,26 @@
+// Problem: 28
+// Status: ToDo
+// Last Modified: Mon, 16 May, 2022
+// Author: Abdullah Leghari
+// Time = ToDo
+
+
+package problems;
+
+public class Problem28 {
+
+	public static void main(String[] args) {
+		
+		int max = 5;
+		int mid = max/2 + (max%2);
+		
+		System.out.println(mid);
+		int arr[][] = new int[max][max];
+		
+		for(int i = 1; i <= max; i++) {
+			
+		}
+
+	}
+
+}
